dc010dd07fbb92adcae583675c91eb1ff7a4577d
2yd <1046787953@qq.com>
2022-09-13 23:57:14 +0800

    eighth commit

--- bot.js
+++ bot.js
@@ -13,4 +13,18 @@ client.on('message',msg => {
     }
 })
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
 client.login(process.env.BOT_TOKEN)
