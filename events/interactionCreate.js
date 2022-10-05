4dee6c055db39f9473d08e5c280fc0d0de8099b2
2yd <1046787953@qq.com>
2022-10-05 20:32:45 +0800

    debug

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,6 +2,8 @@ module.exports = {
 	name: 'interactionCreate',
 	async execute(interaction) {
 		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
-		console.log(interaction.isButton())
+		if (interaction.isButton()) {
+			await interaction.reply('点我干嘛')
+		}
 	},
 };
